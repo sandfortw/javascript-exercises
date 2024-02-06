1d1debde7229a436a575db9c112ec3f89a79a279
Weston Sandfort <sandfortw@gmail.com>
2024-02-05 19:13:32 -0700

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  string2 = '';
+  if (num >= 0){
+    for (let i = 0; i < num; i++){
+      string2 += string;
+    }
+    return string2
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
