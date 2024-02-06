4c7c1a3f7b0c29f83f74eaa9e88eb2782c6198c2
Weston Sandfort <sandfortw@gmail.com>
2024-02-06 13:52:24 -0700

    Complete in-lesson exercise on dom manipulation

---
+++ dom_manipulation/js-file.js
@@ -0,0 +1,30 @@
+const container = document.querySelector('#container');
+
+// a <p> with red text that says “Hey I’m red!”
+const redP = document.createElement('p');
+redP.textContent = 'Hey I\'m red!';
+redP.style.color = 'red';
+container.appendChild(redP);
+
+// an <h3> with blue text that says “I’m a blue h3!”
+const blueH3 = document.createElement('H3');
+blueH3.textContent = 'I’m a blue h3!';
+blueH3.style.color = 'blue';
+container.appendChild(blueH3);
+
+// a <div> with a black border and pink background color with the following elements inside of it:
+// another <h1> that says “I’m in a div”
+// a <p> that says “ME TOO!”
+// Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+
+const pinkDiv = document.createElement('div');
+pinkDiv.setAttribute('style', 'background: pink; border-color: black; border-style: solid');
+const element1 = document.createElement('h1');
+element1.textContent = 'I\’m in a div';
+pinkDiv.appendChild(element1);
+
+const element2 = document.createElement('p');
+element2.textContent = 'ME TOO!';
+
+pinkDiv.appendChild(element2);
+container.appendChild(pinkDiv);
