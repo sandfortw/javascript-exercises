c958ba29e598f2e7c8a27210e1977a819047bfbf
Weston Sandfort <sandfortw@gmail.com>
2024-02-06 14:06:33 -0700

    Add a button on which I practiced js events

--- dom_manipulation/js-file.js
+++ dom_manipulation/js-file.js
@@ -28,3 +28,34 @@ element2.textContent = 'ME TOO!';
 
 pinkDiv.appendChild(element2);
 container.appendChild(pinkDiv);
+
+// const btn = document.querySelector('#btn');
+// btn.onclick = () => alert("Hello World");
+
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', () => {
+//   alert("Hello World");
+// });
+
+// function alertFunction() {
+//   alert("YAY! YOU DID IT!");
+// };
+
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
+const btn = document.querySelector('#btn')
+// btn.onclick = alertFunction;
+btn.addEventListener('click', alertFunction);
+
+// btn.addEventListener('click', function (e) {
+//   console.log(e);
+// });
+
+// btn.addEventListener('click', function (e) {
+//   console.log(e.target);
+// });
+
+btn.addEventListener('click', function (e) {
+  e.target.style.background = 'blue';
+});
