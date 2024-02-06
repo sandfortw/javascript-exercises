0d1efec60fcaf132f6a264cde03f8fd91c6efa1b
Weston Sandfort <sandfortw@gmail.com>
2024-02-05 19:30:30 -0700

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const last_position = string.length - 1;
+  let reversed = '';
+  for (i = last_position; i >= 0; i-- ) {
+    reversed += string[i];
+  }
+  return reversed
 };
 
 // Do not edit below this line
