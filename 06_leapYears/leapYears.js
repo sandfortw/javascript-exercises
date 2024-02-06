d0c6b337e19d2a88639807dd5e4ebdfd541f4b16
Weston Sandfort <sandfortw@gmail.com>
2024-02-05 22:15:04 -0700

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const isDivisibleBy4 = year % 4 === 0;
+  const isDivisibleBy100 = year % 100 === 0;
+  const isDivisibleBy400 = year % 400 === 0;
 
+  if(isDivisibleBy4){
+    if(isDivisibleBy100 && !isDivisibleBy400){
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
