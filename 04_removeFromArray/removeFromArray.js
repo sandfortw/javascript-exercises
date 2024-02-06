7aa2aa05e4c22cc2e515ef44d714db14b99e1507
Weston Sandfort <sandfortw@gmail.com>
2024-02-05 22:06:38 -0700

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
-
+  let array = arguments[0];
+  args_length = Object.keys(arguments).length;
+  for (let i = args_length - 1 ; i >= 1; i--) {
+    array = array.filter((e) => e !== arguments[i]);
+  }
+  return array;
 };
 
 // Do not edit below this line
