bfda763703686c25e11e562bb7a7b30b1b2b3996
Weston Sandfort <sandfortw@gmail.com>
2024-02-06 14:11:45 -0700

    Add event listener for each button in a nodelist

--- dom_manipulation/js-file.js
+++ dom_manipulation/js-file.js
@@ -41,14 +41,14 @@ container.appendChild(pinkDiv);
 //   alert("YAY! YOU DID IT!");
 // };
 
-function alertFunction() {
-  alert("YAY! YOU DID IT!");
-}
-const btn = document.querySelector('#btn')
-// btn.onclick = alertFunction;
-btn.addEventListener('click', alertFunction);
+// function alertFunction() {
+//   alert("YAY! YOU DID IT!");
+// }
+// const btn = document.querySelector('#btn')
+// // btn.onclick = alertFunction;
+// btn.addEventListener('click', alertFunction);
 
-// btn.addEventListener('click', function (e) {
+// // btn.addEventListener('click', function (e) {
 //   console.log(e);
 // });
 
@@ -56,6 +56,18 @@ btn.addEventListener('click', alertFunction);
 //   console.log(e.target);
 // });
 
-btn.addEventListener('click', function (e) {
-  e.target.style.background = 'blue';
+// btn.addEventListener('click', function (e) {
+//   e.target.style.background = 'blue';
+// });
+
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll('button');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener('click', () => {
+    alert(button.id);
+  });
 });
