85fce9f26fecf96b33ef05c839231974fa4f9427
Weston Sandfort <sandfortw@gmail.com>
2024-02-05 21:15:46 -0700

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  const sumOfIntegers = (big, small) => ((big - small + 1) * (small + big))/ 2;
+  const bothIntegers = Number.isInteger(num1) && Number.isInteger(num2);
+  const bothPositive = num1 >=0 && num2 >=0;
+  if(bothIntegers && bothPositive){
+    return num1 > num2 ? sumOfIntegers(num1, num2) : sumOfIntegers(num2, num1)
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
