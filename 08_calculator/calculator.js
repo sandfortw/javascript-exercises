c57b1bd56ccd2622ed5153dbe660ffde2b270242
Weston Sandfort <sandfortw@gmail.com>
2024-02-08 16:45:45 -0700

    Complete challenge 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let num = 0;
+  array.forEach((n) => num += n)
+  return num;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let num = nums.shift();
+  nums.forEach((n) => num *= n)
+  return num;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) {
+    return 1;
+  } else {
+    let factorial = num;
+    for(let i = num; i > 1; i--){
+      factorial *= (i - 1); 
+    } 
+    return factorial;
+  }
 };
 
 // Do not edit below this line
