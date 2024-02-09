89b694a5cb7060d47aa9b2db7915e303b48672c0
Weston Sandfort <sandfortw@gmail.com>
2024-02-08 21:01:38 -0700

    Complete challenge 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
+const reverseString = require('../03_reverseString/reverseString');
 
+const palindromes = function (string) {
+  let normalized = string.toLowerCase().replace(/\W+/g, "")
+  return normalized === reverseString(normalized)
 };
 
 // Do not edit below this line
