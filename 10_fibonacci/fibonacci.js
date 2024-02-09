47d48e282399a2e3db852b2bc0f9c9a3da0c5f63
Weston Sandfort <sandfortw@gmail.com>
2024-02-08 21:29:47 -0700

    Complete challenge 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num == 0) {
+    return 0;
+  }
+  if (num < 0) {
+    return "OOPS";
+  }
+  let fib = [0, 1];
+  for (i = 2; i <= num; i++) {
+    new_num = fib[0] + fib[1];
+    console.log(`new num is ${new_num}`);
+    fib.push(new_num);
+    fib.shift();
+  }
+  return fib[1];
 };
 
 // Do not edit below this line
