f08f133d24b04e2958c6194e4bc670d5ee303bf8
Weston Sandfort <sandfortw@gmail.com>
2024-02-08 21:31:18 -0700

    Format challenge 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,6 @@
 const fibonacci = function (num) {
-  if (num == 0) {
-    return 0;
-  }
-  if (num < 0) {
-    return "OOPS";
-  }
+  if (num == 0) return 0;
+  if (num < 0) return "OOPS";
   let fib = [0, 1];
   for (i = 2; i <= num; i++) {
     new_num = fib[0] + fib[1];
