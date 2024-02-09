dad0e227ab87eda73627619ba67fa0c86b3e4f4d
Weston Sandfort <sandfortw@gmail.com>
2024-02-08 22:12:20 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  const ageSorted = people.sort((a, b) => {
+    const currentYear = new Date().getFullYear();
+    [a, b].forEach(x => "yearOfDeath" in x ? "" : x["yearOfDeath"] = currentYear);
+    return a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth);
+  });
+  return ageSorted.pop();
 };
 
 // Do not edit below this line
